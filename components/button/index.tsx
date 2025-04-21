61c88d2b157a16978b29f10fb8ae37f7ef951e1f
Nakhon Viriyarodjanawut <nkn.vrw@gmail.com>
2025-04-21 12:00:07 +0700

    1

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -2,7 +2,7 @@
 export function Button({ children }: { children: React.ReactNode }) {
   return (
     <button className="px-4 py-2 rounded bg-blue-600 text-white">
-      {children}
+      2{children}
     </button>
   );
 }
