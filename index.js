f9d2717da553cdfddde296e5dcfd80b22eb9fff9
Nakhon Viriyarodjanawut <nkn.vrw@gmail.com>
2025-04-21 13:47:32 +0700

    1

--- index.js
+++ index.js
@@ -32,29 +32,43 @@ const __dirname = path.dirname(__filename);
 const args = process.argv.slice(2);
 const command = args[0]; // เช่น "add"
 const componentName = args[1]; // เช่น "button"
+console.log('command', command)
+if (command === "init") {
+  const data = {
+    name: "ChatGPT",
+    type: "AI",
+    language: "Node.js",
+  };
 
-if (command !== "add" || !componentName) {
+  fs.writeFileSync(
+    "data.json",
+    JSON.stringify(data, null, 2),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("JSON data saved successfully!");
+    }
+  );
+} else if (!(command !== "add" || !componentName)) {
+  const baseDir = path.join(__dirname, "components");
+  const templatePath = path.resolve(baseDir, componentName);
+
+  if (!fs.existsSync(templatePath) || !templatePath.startsWith(baseDir)) {
+    console.log(`❌ Component "${componentName}" not found.`);
+    process.exit(1);
+  }
+
+  const targetPath = path.join(process.cwd(), "components", componentName);
+
+  await fs.copy(templatePath, targetPath);
+
+  console.log(
+    `✅ Added component "${componentName}" to ./components/${componentName}`
+  );
+} else {
   console.log("Usage: npx github:<user>/<repo> add <component>");
   process.exit(1);
 }
-
-// templatePath.startsWith(path.join(__dirname, "components"))
-
-const baseDir = path.join(__dirname, "components");
-const templatePath = path.resolve(baseDir, componentName);
-const targetPath = path.join(process.cwd(), "components", componentName);
-
-console.log("baseDir", baseDir);
-console.log("targetPath", targetPath);
-console.log("templatePath", templatePath);
-
-if (!fs.existsSync(templatePath) || !templatePath.startsWith(baseDir)) {
-  console.log(`❌ Component "${componentName}" not found.`);
-  process.exit(1);
-}
-
-await fs.copy(templatePath, targetPath);
-
-console.log(
-  `✅ Added component "${componentName}" to ./components/${componentName}`
-);
