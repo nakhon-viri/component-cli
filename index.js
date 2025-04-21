fd70f9de87eac0ff5732129ab9ae1fde8f7ebf21
Nakhon Viriyarodjanawut <nkn.vrw@gmail.com>
2025-04-21 14:03:22 +0700

    1

--- index.js
+++ index.js
@@ -32,47 +32,69 @@ const __dirname = path.dirname(__filename);
 const args = process.argv.slice(2);
 const command = args[0]; // เช่น "add"
 const componentName = args[1]; // เช่น "button"
-if (command === "init") {
-  console.log("init");
-  const data = {
-    aliases: {
-      default: "componets",
-      path: {
-        components: "src/components",
-      },
-    },
-  };
-
-  fs.writeFileSync(
-    "component-cli.config.json",
-    JSON.stringify(data, null, 2),
-    "utf8",
-    (err) => {
-      if (err) {
-        console.error("Error writing file:", err);
-        return;
-      }
-      console.log("JSON data saved successfully!");
-    }
-  );
-} else if (!(command !== "add" || !componentName)) {
-  const baseDir = path.join(__dirname, "components");
-  const templatePath = path.resolve(baseDir, componentName);
-  console.log("baseDir", baseDir);
-  console.log("templatePath", templatePath);
-  if (!fs.existsSync(templatePath) || !templatePath.startsWith(baseDir)) {
-    console.log(`❌ Component "${componentName}" not found.`);
-    process.exit(1);
-  }
-  console.log('process.cwd()', process.cwd())
-  const targetPath = path.join(process.cwd(), "components", componentName);
-  console.log("targetPath", targetPath);
-  await fs.copy(templatePath, targetPath);
-
-  console.log(
-    `✅ Added component "${componentName}" to ./components/${componentName}`
-  );
-} else {
+if (command !== "add" || !componentName) {
   console.log("Usage: npx github:<user>/<repo> add <component>");
   process.exit(1);
 }
+
+const baseDir = path.join(__dirname, "components");
+const templatePath = path.resolve(baseDir, componentName);
+console.log("baseDir", baseDir);
+console.log("templatePath", templatePath);
+if (!fs.existsSync(templatePath) || !templatePath.startsWith(baseDir)) {
+  console.log(`❌ Component "${componentName}" not found.`);
+  process.exit(1);
+}
+console.log("process.cwd()", process.cwd());
+const targetPath = path.join(process.cwd(), "components", componentName);
+console.log("targetPath", targetPath);
+await fs.copy(templatePath, targetPath);
+
+console.log(
+  `✅ Added component "${componentName}" to ./components/${componentName}`
+);
+
+// if (command === "init") {
+//   console.log("init");
+//   const data = {
+//     aliases: {
+//       default: "componets",
+//       path: {
+//         components: "src/components",
+//       },
+//     },
+//   };
+
+//   fs.writeFileSync(
+//     "component-cli.config.json",
+//     JSON.stringify(data, null, 2),
+//     "utf8",
+//     (err) => {
+//       if (err) {
+//         console.error("Error writing file:", err);
+//         return;
+//       }
+//       console.log("JSON data saved successfully!");
+//     }
+//   );
+// } else if (!(command !== "add" || !componentName)) {
+//   const baseDir = path.join(__dirname, "components");
+//   const templatePath = path.resolve(baseDir, componentName);
+//   console.log("baseDir", baseDir);
+//   console.log("templatePath", templatePath);
+//   if (!fs.existsSync(templatePath) || !templatePath.startsWith(baseDir)) {
+//     console.log(`❌ Component "${componentName}" not found.`);
+//     process.exit(1);
+//   }
+//   console.log('process.cwd()', process.cwd())
+//   const targetPath = path.join(process.cwd(), "components", componentName);
+//   console.log("targetPath", targetPath);
+//   await fs.copy(templatePath, targetPath);
+
+//   console.log(
+//     `✅ Added component "${componentName}" to ./components/${componentName}`
+//   );
+// } else {
+//   console.log("Usage: npx github:<user>/<repo> add <component>");
+//   process.exit(1);
+// }
